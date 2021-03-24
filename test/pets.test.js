cb5fd492f35889ed9c9da3e2ac672ad818b8235d
olegz-codefresh <58692269+olegz-codefresh@users.noreply.github.com>
2021-03-24 23:03:02 +0200

    Update pets.test.js

--- test/pets.test.js
+++ test/pets.test.js
@@ -18,22 +18,22 @@ describe('GET /', () => {
     });
 });
 
-describe('/POST', () => {
-    it('should create pet with valid request', (done) => {
-        chai
-            .request(server)
-            .post('/addPet')
-            .send({
-                name: 'Molly',
-                colour: 'browne',
-                age: '5 year',
-                breed: 'Border Collie',
-                owner: 2,
-            })
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                expect(res.body).to.have.all.keys('id', 'owner', 'age', 'breed', 'colour', 'name');
-                done();
-            });
-    });
-});
+// describe('/POST', () => {
+//     it('should create pet with valid request', (done) => {
+//         chai
+//             .request(server)
+//             .post('/addPet')
+//             .send({
+//                 name: 'Molly',
+//                 colour: 'browne',
+//                 age: '5 year',
+//                 breed: 'Border Collie',
+//                 owner: 2,
+//             })
+//             .end((err, res) => {
+//                 expect(res).to.have.status(200);
+//                 expect(res.body).to.have.all.keys('id', 'owner', 'age', 'breed', 'colour', 'name');
+//                 done();
+//             });
+//     });
+// });
